896a511435e2fdcc41a184a229ecf1d7699ad7a1
Timothée Péron <3842709-timothee-peron@users.noreply.gitlab.com>
2023-12-01 06:28:50 +0100

    2023-01

--- 2023/01.py
+++ 2023/01.py
@@ -1,8 +1,51 @@
 import utils
 
+from sortedcontainers import SortedDict
+
 utils.DEBUG = True
 utils.DEBUG = False
 utils.printInfo()
 
 inputLines = utils.fileToLines(utils.inputFilePath())
-print(inputLines)
+
+nums = [list(map(int, filter(lambda v: v.isdigit(), list(x)))) for x  in inputLines]
+calVals = []
+for val in nums:
+    calVals.append(val[0] * 10 + val[-1])
+print(sum(calVals))
+
+searches = {
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    'one':1,
+    'two':2,
+    'three':3,
+    'four':4,
+    'five':5,
+    'six':6,
+    'seven':7,
+    'eight':8,
+    'nine':9
+}
+
+calVals = []
+for line in inputLines:
+    numbers = SortedDict()
+
+    for k, v in searches.items():
+        f = line.find(k)
+        l = line.rfind(k)
+
+        if f is not -1:
+            numbers[f] = v
+            numbers[l] = v
+    keys = numbers.keys()
+    calVals.append(numbers[keys[0]] * 10 + numbers[keys[ - 1]])
+print(sum(calVals))
