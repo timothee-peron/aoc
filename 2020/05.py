cc2b8795ed2c0bed6aafd697f1ab3b6aabff72dd
Timothée Péron <3842709-timothee-peron@users.noreply.gitlab.com>
2023-12-01 20:50:48 +0100

    2020-05 2020-06

---
+++ 2020/05.py
@@ -0,0 +1,49 @@
+import utils
+
+utils.DEBUG = True
+utils.DEBUG = False
+utils.printInfo()
+
+inputLines = utils.linesToItems(utils.fileToLines(utils.inputFilePath()), list)
+
+def getRow(seat):
+    l = seat[0:7][::-1]
+    r = 0
+    b = 1
+    for s in l:
+        r += b * ( 1 if s =='B' else 0)
+        b *= 2
+    return r
+
+def getCol(seat):
+    l = seat[-3:][::-1]
+    r = 0
+    b = 1
+    for s in l:
+        r += b * ( 1 if s =='R' else 0)
+        b *= 2
+    return r
+
+def getId(seat):
+    return getRow(seat) * 8 + getCol(seat)
+
+assert(getRow(list('FBFBBFFRLR')) == 44)
+assert(getCol(list('FBFBBFFRLR')) == 5)
+assert(getId(list('FBFBBFFRLR')) == 357)
+assert(getId(list('BFFFBBFRRR')) == 567)
+assert(getId(list('FFFBBBFRRR')) == 119)
+assert(getId(list('BBFFBBFRLL')) == 820)
+
+allIds = list(map(getId, inputLines))
+print(max(allIds))
+
+allIds.sort()
+lastId = None
+for x in allIds:
+    if lastId is None:
+        lastId = x
+        continue
+    if x != lastId + 1:
+        print(lastId + 1)
+        break
+    lastId = x
---
+++ 2020/06.py
@@ -0,0 +1,29 @@
+import utils
+
+utils.DEBUG = True
+utils.DEBUG = False
+utils.printInfo()
+
+inputLines = utils.fileToLString(utils.inputFilePath()).split('\n\n')
+
+charsSet = [set(list(l)) for l in inputLines]
+for s in charsSet:
+    s.discard('\n')
+
+print(sum(map(len, charsSet)))
+
+cnts = []
+for line in inputLines:
+    answers = line.split('\n')
+    s = None
+
+    for answer in answers:
+        _s = set(list(answer))
+        if s is None:
+            s = _s
+        else:
+            s = s & _s
+    
+    cnts.append(len(s))
+
+print(sum(cnts))
