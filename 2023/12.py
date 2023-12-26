13a960ca25832d0da383ad873a0ff6af2b435ee1
Timothée Péron <3842709-timothee-peron@users.noreply.gitlab.com>
2023-12-26 17:17:14 +0100

    2023-12-p2 dynamic prg + refactor

--- 2023/12.py
+++ 2023/12.py
@@ -1,73 +1,52 @@
 import utils
 import itertools
-from collections import Counter
 from functools import cache
 
 # utils.DEBUG = True
 utils.printInfo()
 
-data = list(map(lambda x: x.split(' '), filter(lambda x: x != "", utils.fileToLString(utils.inputFilePath()).split('\n'))))
-for z in data:
-    x, y = z
-    x = tuple(x)
-    y = tuple(map(int, y.split(',')))
-    z[0] = x
-    z[1] = y
-
-# part 1 (bruteforce)
+data = utils.fileToLines(utils.inputFilePath())
 
 @cache
-def replaceInStrArray(str1, replArr):
-    str2 = []
-    i = 0
-    for c in str1:
-        if c == '?':
-            str2.append(replArr[i])
-            i += 1
-        else:
-            str2.append(c)
-    return tuple(str2)
+def dynamicPrg(chars, states):
+    total = 0
 
-# get damaged groups
-@cache
-def strDmg(_str):
-    _dmg = []
-    status = None
-    for c in _str:
-        if status == None:
-            status = c
-            if status == '#':
-                _dmg.append(1)
-            continue
+    if len(chars) == 0:
+        if len(states) == 0:
+            return 1
+        return 0
 
-        newStatus = c
-        if newStatus == '#':
-            if newStatus == status:
-                _dmg[-1] += 1
-            else:
-                _dmg.append(1)
+    if len(states) == 0:
+        if "#" in chars:
+            return 0
+        return 1
 
-        status = newStatus
-    return tuple(_dmg)
+    if len(chars) < sum(states) + len(states) - 1:
+        return 0
 
-i = 0
-valid = 0
-for chars, damaged in data:
-    cnt = Counter(chars)
-    repl = itertools.product(['.', '#'], repeat=cnt['?'])
-    
-    # print(chars)
-    for _repl in repl:
-        candidate = replaceInStrArray(chars, _repl)
-        if strDmg(candidate) == damaged:
-            valid += 1
-    # repl = list(repl)
-    # print(repl)
+    if chars[0] in ".?":
+        total += dynamicPrg(chars[1:], states)
 
-    i+=1
-    print(i)
+    n = states[0]
+    if (
+        chars[0] in "#?"
+        and "." not in chars[:n]
+        and (len(chars) == n or chars[n] in ".?")
+    ):
+        total += dynamicPrg(chars[n + 1 :], states[1:])
 
-print(valid)
-if not utils.DEBUG:
-    assert valid == 7379
+    return total
 
+
+def solve(lines, fold):
+    count = 0
+    for line in lines:
+        chars, states = line.split(" ")
+        chars = "?".join([chars] * fold)
+        states = tuple(int(n) for n in states.split(",")) * fold
+        count += dynamicPrg(chars, states)
+    return count
+
+
+print(solve(data, 1))
+print(solve(data, 5))
